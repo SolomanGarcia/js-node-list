8fdfab55901802b2ec02117e4ebb765a90ce8204
Soloman Garcia <soloman.garcia@gmail.com>
2020-06-25 08:45:26 -0600

    work on fs.readdir function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const fs = require("fs");
+
+fs.readdir(".", (err, filenames) => {
+  // EITHER
+  // err === an error object, which means something went wronge
+  // OR
+  // err === null, which means everything is OK
+
+  if (err) {
+    // error handling code here
+    console.log(err);
+  }
+
+  console.log(filenames);
+});
